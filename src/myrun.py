9edbe19c5da6777d0a1dd40fe6bfa5217be22364
root <root@ip-172-31-10-160.us-west-2.compute.internal>
2016-07-21 23:58:04 +0000

    faster data loading

--- src/myrun.py
+++ src/myrun.py
@@ -1,6 +1,9 @@
 from network3 import *
 mini_batch_size = 10
-training_data, validation_data, test_data = network3.load_all_data('/mnt/captchas')
+import theano.misc.pkl_utils
+with open('/mnt/data.zip','rb') as f:
+  training_data, validation_data, test_data = theano.misc.pkl_utils.load(f)
+#training_data, validation_data, test_data = network3.load_all_data('/mnt/captchas')
 net = Network([                         
         ConvPoolLayer(image_shape=(mini_batch_size, 1, 70, 200), 
                       filter_shape=(20, 1, 5, 5), 
