d773c8f341d15dd85ca51a816313e398c8772e33
Richard Klafter <rpklafter@yahoo.com>
2016-07-21 17:08:44 -0700

    bug fix
    bug fix

--- src/myrun.py
+++ src/myrun.py
@@ -4,11 +4,11 @@ import theano.misc.pkl_utils
 with open('/mnt/data.zip','rb') as f:
   training_data, validation_data, test_data = theano.misc.pkl_utils.load(f)
 #training_data, validation_data, test_data = network3.load_all_data('/mnt/captchas')
-net = Network([                         
-        ConvPoolLayer(image_shape=(mini_batch_size, 1, 70, 200), 
-                      filter_shape=(20, 1, 5, 5), 
+net = Network([                                 
+        ConvPoolLayer(image_shape=(mini_batch_size, 1, 70, 200),
+                      filter_shape=(20, 1, 5, 5),
                       poolsize=(2, 2)),
-        FullyConnectedLayer(n_in=20*12*12, n_out=100),
-        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+        FullyConnectedLayer(n_in=20*(200-4)/2*(70-4)/2, n_out=100),
+        SoftmaxLayer(n_in=100, n_out=26)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.1, 
             validation_data, test_data)
