50d213c90b6eb157f776f8521cd26cf0e49f93b7
FreeTheNation <rpklafter@yahoo.com>
2016-07-23 23:43:41 -0700

    tweaks

--- src/fabfile.py
+++ src/fabfile.py
@@ -4,5 +4,5 @@ def setup():
     sudo('umount /mnt')
     sudo('chmod 0777 /mnt')
     run('scp freethenation@icyego.com:~/data_small.zip /mnt/')
-    run('git clone https://github.com/freethenation/neural-networks-and-deep-learning.git')
+    run('cd /mnt; git clone https://github.com/freethenation/neural-networks-and-deep-learning.git')
     
