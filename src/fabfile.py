b037666b4758bcf5971752b227a549a851f1162c
FreeTheNation <rpklafter@yahoo.com>
2016-07-23 23:36:49 -0700

    added fab file

--- src/fabfile.py
+++ src/fabfile.py
@@ -3,6 +3,6 @@ from fabric.operations import sudo, run
 def setup():
     sudo('umount /mnt')
     sudo('chmod 0777 /mnt')
-    run('scp freethenation@icyego.com:~/data_small.zip ./')
+    run('scp freethenation@icyego.com:~/data_small.zip /mnt/')
     run('git clone https://github.com/freethenation/neural-networks-and-deep-learning.git')
     
