92244c640cc8e9fce1e32a9b7d369065c0b798d4
Richard Klafter <rpklafter@yahoo.com>
2016-07-23 22:47:02 -0700

    Update myrun.py

--- src/myrun.py
+++ src/myrun.py
@@ -41,7 +41,21 @@ net = Network([
         FullyConnectedLayer(n_in=40 * ((36-4)/2-4)/2 * ((100-4)/2-4)/2, n_out=100),
         SoftmaxLayer(n_in=100, n_out=26)], mini_batch_size)
 
-net.SGD(training_data, 200, mini_batch_size, 0.01,
+net.SGD(training_data, 200, mini_batch_size, 0.02,
             validation_data, test_data)
 
+#try 4
+from network3 import ReLU
+net = Network([                                   
+        ConvPoolLayer(image_shape=(mini_batch_size, 1, 36, 100),
+                      filter_shape=(40, 1, 5, 5),
+                      poolsize=(2, 2), activation_fn=ReLU),
+        ConvPoolLayer(image_shape=(mini_batch_size, 40, (36-4)/2, (100-4)/2),
+                      filter_shape=(40, 40, 5, 5),
+                      poolsize=(2, 2), activation_fn=ReLU),
+        FullyConnectedLayer(n_in=40 * ((36-4)/2-4)/2 * ((100-4)/2-4)/2, n_out=100, activation_fn=ReLU),
+        SoftmaxLayer(n_in=100, n_out=26)], mini_batch_size)
+
+net.SGD(training_data, 200, mini_batch_size, 0.02,
+            validation_data, test_data, lmbda=0.1) #might remove lmbda if it does work well
 
